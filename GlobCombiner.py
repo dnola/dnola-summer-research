078aeb429fcb0b422589f9af787d6aceb0d11a01
davidnola <dnola@scu.edu>
2014-06-28 19:57:19 -0700

    Adding GlobCombiner

---
+++ GlobCombiner.py
@@ -0,0 +1,28 @@
+__author__ = 'davidnola'
+
+import glob
+import cPickle
+
+results = []
+names = []
+for f in glob.glob('Results/*.pkl'):
+    pkl = cPickle.load(open(f, 'rb'))
+    for p, v in pkl:
+        (name, result) = (p, v)
+        results.append(result)
+        names.append(name)
+
+
+final_text = ""
+riter = iter(results)
+
+for name in names:
+    next = str(round(riter.next(), 5))
+    final_text += name + "," + next + ", " + next + "\n"
+
+
+print final_text
+
+f = open('finalSubmitSingle.csv', 'a')
+f.write(final_text)
+f.close()
